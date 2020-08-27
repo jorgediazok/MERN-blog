31ec15a18ab3c9296da05bd1bb11a20a9544c4e1
Jorge <jorgediazok@gmail.com>
2020-08-27 03:47:39 -0300

    page renderer. Bug on it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import PageRenderer from './Page-Renderer';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route
+            // @ts-ignore
+            component={() => 404}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const navLinks = [
   {
@@ -25,7 +26,9 @@ const Navigation = (props) => {
       <span>JDW International</span>
       <ul>
         {navLinks.map((link, index) => (
-          <li key={index}>{link.title}</li>
+          <li key={index}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
         ))}
       </ul>
     </nav>
