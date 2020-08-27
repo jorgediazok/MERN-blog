038d3d8fbc2353c556ffce0247d6d3fd9e261e5c
Jorge <jorgediazok@gmail.com>
2020-08-27 01:49:48 -0300

    added scss variables + creating navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'Blog',
+    path: '/blog',
+  },
+  {
+    title: 'Contact Us',
+    path: '/contact-us',
+  },
+  {
+    title: 'Login',
+    path: '/login',
+  },
+];
+
 const Navigation = (props) => {
-  return <nav>test</nav>;
+  return (
+    <nav className="site-navigation">
+      <span>JDW International</span>
+      <ul>
+        {navLinks.map((link, index) => (
+          <li key={index}>{link.title}</li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
