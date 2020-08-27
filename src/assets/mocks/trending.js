5a4ac6a38141aa59ccba8499ec0013015eaeca2c
Jorge <jorgediazok@gmail.com>
2020-08-27 20:53:34 -0300

    created mocks

---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,32 @@
+import moment from 'moment';
+
+export default [
+  {
+    title: 'Sofware Engineer Salary in 2020',
+    date: moment().format('MMM DD YYYY'),
+    categories: ['Tech Culture'],
+    link: '#',
+    image: 'money.jpg',
+  },
+  {
+    title: 'GraphQL vs REST',
+    date: moment().format('MMM DD YYYY'),
+    categories: ['React', 'Javascript'],
+    link: '#',
+    image: 'apollo_graphql.jpg',
+  },
+  {
+    title: 'A Day in the Life of a Programmer',
+    date: moment().format('MMM DD YYYY'),
+    categories: ['Tech Culture'],
+    link: '#',
+    image: 'puzzled_programmer.jpg',
+  },
+  {
+    title: 'Brain Hacks for Learning to Program',
+    date: moment().format('MMM DD YYYY'),
+    categories: ['Brain Health'],
+    link: '#',
+    image: 'neuron.jpg',
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return <section className="container home"></section>;
 };
 
 export default Home;
