2cf4be2cfd21646cfc15d7c3d4d12e273f27b74f
Jorge <jorgediazok@gmail.com>
2020-08-28 01:59:31 -0300

    making de scss of the grid

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { categoryColors } from './styles';
 
 export default function MasonryPost({ post, tagsOnTop }) {
-  const style = {
+  const imageBackground = {
     backgroundImage: `url("${require(`../../assets/images/${post.image}`)}`,
   };
+
+  const style = { ...imageBackground, ...post.style };
 
   return (
     <a href={post.link} className="masonry-post overlay" style={style}>
       <div className="image-text">
+        <div className="tags-container">
+          {post.categories.map((tag, ind) => (
+            <span
+              key={ind}
+              className="tag"
+              style={{ backgroundColor: categoryColors[tag] }}
+            >
+              {tag.toUpperCase()}
+            </span>
+          ))}
+        </div>
         <h2 className="image-title">{post.title}</h2>
+        <span className="image-date">{post.date}</span>
       </div>
     </a>
   );
